e5df4dd71417490b969a0282f327250dd6a08df0
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2025-04-22 13:17:15 -0500

    fix formatting

--- src/main/java/io/github/theluca98/creativeheads/util/ItemBuilder.java
+++ src/main/java/io/github/theluca98/creativeheads/util/ItemBuilder.java
@@ -9,11 +9,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with this program.	If not, see <https://www.gnu.org/licenses/>.
  */
 
 package io.github.theluca98.creativeheads.util;
@@ -42,112 +42,112 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 public class ItemBuilder {
 
-    private final ItemStack item;
-    private final ItemMeta meta;
+		private final ItemStack item;
+		private final ItemMeta meta;
 
-    private ItemBuilder(ItemStack item) {
-        this.item = item;
-        this.meta = item.getItemMeta();
-    }
+		private ItemBuilder(ItemStack item) {
+				this.item = item;
+				this.meta = item.getItemMeta();
+		}
 
-    public static ItemBuilder of(Material material, int amount) {
-        return new ItemBuilder(new ItemStack(material, amount));
-    }
+		public static ItemBuilder of(Material material, int amount) {
+				return new ItemBuilder(new ItemStack(material, amount));
+		}
 
-    public static ItemBuilder of(Material material) {
-        return new ItemBuilder(new ItemStack(material));
-    }
+		public static ItemBuilder of(Material material) {
+				return new ItemBuilder(new ItemStack(material));
+		}
 
-    public static ItemBuilder from(ItemStack item) {
-        return new ItemBuilder(item.clone());
-    }
+		public static ItemBuilder from(ItemStack item) {
+				return new ItemBuilder(item.clone());
+		}
 
-    public ItemBuilder withName(String name) {
-        meta.setDisplayName(name);
-        return this;
-    }
+		public ItemBuilder withName(String name) {
+				meta.setDisplayName(name);
+				return this;
+		}
 
-    public ItemBuilder withLore(String... lore) {
-        meta.setLore(Arrays.asList(lore));
-        return this;
-    }
+		public ItemBuilder withLore(String... lore) {
+				meta.setLore(Arrays.asList(lore));
+				return this;
+		}
 
-    public ItemBuilder withHiddenAttributes() {
-        meta.addItemFlags(ItemFlag.values());
-        return this;
-    }
-  
-    @SneakyThrows
-    public ItemBuilder withCustomSkullProfile(PlayerProfile profile) {
-        checkArgument(meta instanceof SkullMeta, "Not a player head item");
-        ((SkullMeta) meta).setOwnerProfile(profile);
-        return this;
-    }
+		public ItemBuilder withHiddenAttributes() {
+				meta.addItemFlags(ItemFlag.values());
+				return this;
+		}
 
-    public ItemBuilder withCustomSkullTexture(String textureUrl) {
-        var profile = Bukkit.createPlayerProfile(UUID.randomUUID(), "CreativeHeads");
-        var textures = profile.getTextures();
-        try {        
-            textures.setSkin(new URL(textureUrl));
-            profile.setTextures(textures);
-            return withCustomSkullProfile(profile);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+		@SneakyThrows
+		public ItemBuilder withCustomSkullProfile(PlayerProfile profile) {
+				checkArgument(meta instanceof SkullMeta, "Not a player head item");
+				((SkullMeta) meta).setOwnerProfile(profile);
+				return this;
+		}
 
-    public String getOnlineUUID(String username) {
-        try {
-            URL uuidUrl = new URL("https://api.mojang.com/users/profiles/minecraft/" + username);
-            InputStreamReader uuidReader = new InputStreamReader(uuidUrl.openStream());
-            JsonObject uuidJson = JsonParser.parseReader(uuidReader).getAsJsonObject();
-            return uuid = uuidJson.get("id").getAsString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+		public ItemBuilder withCustomSkullTexture(String textureUrl) {
+				var profile = Bukkit.createPlayerProfile(UUID.randomUUID(), "CreativeHeads");
+				var textures = profile.getTextures();
+				try {
+						textures.setSkin(new URL(textureUrl));
+						profile.setTextures(textures);
+						return withCustomSkullProfile(profile);
+				} catch (MalformedURLException e) {
+						e.printStackTrace();
+						return null;
+				}
+		}
 
-    public String getSkinUrl(String uuid) {
-        try {
-            URL profileUrl = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid);
-            InputStreamReader profileReader = new InputStreamReader(profileUrl.openStream());
-            JsonObject profileJson = JsonParser.parseReader(profileReader).getAsJsonObject();
-            JsonObject properties = profileJson.get("properties").getAsJsonArray().get(0).getAsJsonObject();
-            String texture = properties.get("value").getAsString();
+		public String getOnlineUUID(String username) {
+				try {
+						URL uuidUrl = new URL("https://api.mojang.com/users/profiles/minecraft/" + username);
+						InputStreamReader uuidReader = new InputStreamReader(uuidUrl.openStream());
+						JsonObject uuidJson = JsonParser.parseReader(uuidReader).getAsJsonObject();
+						return uuid = uuidJson.get("id").getAsString();
+				} catch (Exception e) {
+						e.printStackTrace();
+						return null;
+				}
+		}
 
-            JsonObject textureJson = JsonParser.parseString(new String(Base64.getDecoder().decode(texture))).getAsJsonObject();
-            return textureJson.get("textures").getAsJsonObject().get("SKIN").getAsJsonObject().get("url").getAsString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+		public String getSkinUrl(String uuid) {
+				try {
+						URL profileUrl = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid);
+						InputStreamReader profileReader = new InputStreamReader(profileUrl.openStream());
+						JsonObject profileJson = JsonParser.parseReader(profileReader).getAsJsonObject();
+						JsonObject properties = profileJson.get("properties").getAsJsonArray().get(0).getAsJsonObject();
+						String texture = properties.get("value").getAsString();
 
-    // @SuppressWarnings("deprecation")
-    public ItemBuilder withCustomSkullOwner(String playerName) {
-        checkArgument(meta instanceof SkullMeta, "Not a player head item");
-        // ((SkullMeta) meta).setOwningPlayer(Bukkit.getOfflinePlayer(playerName));
-        var playerUUID = getOnlineUUID(playerName);
-        var textureUrl = getSkinURL(playerUUID);
+						JsonObject textureJson = JsonParser.parseString(new String(Base64.getDecoder().decode(texture))).getAsJsonObject();
+						return textureJson.get("textures").getAsJsonObject().get("SKIN").getAsJsonObject().get("url").getAsString();
+				} catch (Exception e) {
+						e.printStackTrace();
+						return null;
+				}
+		}
 
-        var profile = Bukkit.createPlayerProfile(getOnlineUUID(playerName), playerName);
-        var textures = profile.getTextures();
-        try {        
-            textures.setSkin(new URL(textureUrl));
-            profile.setTextures(textures);
-            return withCustomSkullProfile(profile);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return this;
-    }
+		// @SuppressWarnings("deprecation")
+		public ItemBuilder withCustomSkullOwner(String playerName) {
+				checkArgument(meta instanceof SkullMeta, "Not a player head item");
+				// ((SkullMeta) meta).setOwningPlayer(Bukkit.getOfflinePlayer(playerName));
+				var playerUUID = getOnlineUUID(playerName);
+				var textureUrl = getSkinURL(playerUUID);
 
-    public ItemStack build() {
-        item.setItemMeta(meta);
-        return item;
-    }
+				var profile = Bukkit.createPlayerProfile(getOnlineUUID(playerName), playerName);
+				var textures = profile.getTextures();
+				try {
+						textures.setSkin(new URL(textureUrl));
+						profile.setTextures(textures);
+						return withCustomSkullProfile(profile);
+				} catch (MalformedURLException e) {
+						e.printStackTrace();
+						return null;
+				}
+				return this;
+		}
+
+		public ItemStack build() {
+				item.setItemMeta(meta);
+				return item;
+		}
 
 }
